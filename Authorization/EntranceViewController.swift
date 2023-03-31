1ba198d45821acb3d1b81643116f713ae2cc5174
Baranov Roman <palamarmle@gmail.com>
2023-03-31 22:04:59 +0300

    Test

---
+++ Authorization/EntranceViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EntranceViewController.swift
+//  Authorization
+//
+//  Created by Рома Баранов on 31.03.2023.
+//
+
+import UIKit
+
+class EntranceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func returnsOnButtonСlick(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+}
