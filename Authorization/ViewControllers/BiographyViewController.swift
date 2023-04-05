e365c0912a7bb95978d417b190b4e75040aa483e
Baranov Roman <palamarmle@gmail.com>
2023-04-06 00:49:38 +0300

    Made the classes final

--- Authorization/ViewControllers/BiographyViewController.swift
+++ Authorization/ViewControllers/BiographyViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BiographyViewController: UIViewController {
+final class BiographyViewController: UIViewController {
     @IBOutlet var biographyLabel: UILabel!
     
     var biography = "vhjhjhg"
